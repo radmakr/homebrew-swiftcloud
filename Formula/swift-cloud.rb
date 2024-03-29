caf64aeea2b9d5f8e4e7aab05e5bdb24328ba334
Thomas Rademaker <thomas@sparrowtek.com>
2024-03-29 19:19:18 -0400

    tar

--- Formula/swift-cloud.rb
+++ Formula/swift-cloud.rb
@@ -1,7 +1,7 @@
 class SwiftCloud < Formula
     desc "A CLI to work with Swift Cloud"
     homepage "https://swift.cloud"
-    url "https://github.com/radmakr/SwiftCloudCLI/releases/download/0.0.1/swiftcloud.zip"
+    url "https://github.com/radmakr/SwiftCloudCLI/archive/refs/tags/0.0.1.tar.gz"
     sha256 "884013973dc177e7bf151836073b71dde4270a114be110d5e88903331c0299d3"
   
     def install
