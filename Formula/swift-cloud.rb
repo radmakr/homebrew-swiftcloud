6c4dbdc5a8eacaa9d343850ac43b3e0967cec9e9
Thomas Rademaker <thomas@sparrowtek.com>
2024-03-29 18:45:42 -0400

    change sha

--- Formula/swift-cloud.rb
+++ Formula/swift-cloud.rb
@@ -2,7 +2,7 @@ class SwiftCloud < Formula
     desc "A CLI to work with Swift Cloud"
     homepage "https://swift.cloud"
     url "https://github.com/radmakr/SwiftCloudCLI/releases/download/0.0.1/swiftcloud.zip"
-    sha256 "884013973dc177e7bf151836073b71dde4270a114be110d5e88903331c0299d3"
+    sha256 "884013973dc177e7bf151836073b71dde4270a114be110d5e88903331c0299d3  swiftcloud.zip"
   
     def install
       bin.install "swiftcloud"
